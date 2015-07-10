a82b5c0c78270f68d3442a7ed1fb63cc7ba5e2ef
Mannuel Ferreira <mannuel@wwc.co.za>
2015-07-10 22:40:30 +0200

    step 7

--- public/scripts/tutorial1.jsx
+++ public/scripts/tutorial1.jsx
@@ -1,8 +1,44 @@
+var CommentList = React.createClass({
+    render: function() {
+        return (
+            <div className="commentList">
+                <Comment author="Mannuel Ferreira">Wow this is not bad at all.</Comment>
+                <Comment author="Denton Pretorius">Yeah I agree.</Comment>
+            </div>
+        );
+    }
+});
+
+var CommentForm = React.createClass({
+    render: function() {
+        return (
+            <div className="commentForm">
+                Hello, world! I am a CommentForm.
+            </div>
+        );
+    }
+});
+
+var Comment = React.createClass({
+   render: function() {
+       return (
+           <div className="comment">
+               <h2 className="commentAuthor">
+                   {this.props.author}
+               </h2>
+               {marked(this.props.children.toString())}
+           </div>
+       );
+   }
+});
+
 var CommentBox = React.createClass({
    render: function() {
        return (
            <div className="commentBox">
-               Hello, world! I am a CommentBox.
+               <h1>Comments</h1>
+               <CommentList/>
+               <CommentForm/>
            </div>
        );
    }
